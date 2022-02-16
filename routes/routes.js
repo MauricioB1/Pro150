3bcc938cdfd6fa5ae133d5d6b99674957b2072e8
MauricioB1 <morriss3031@gmail.com>
2022-02-15 18:43:49 -0700

    Added databse code to routes file

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,10 @@
+//MongoDB dependencies
+const { MongoClient, ObjectId } = require("mongodb");
 
+//MongoDB connection variables
+const client = new MongoClient("mongodb+srv://PRO150:pass@cluster0.k7uok.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+const db = client.db("PRO150");
+const collection = db.collection("User");
 
 exports.index = async (req, res) => {
     res.render("index", {
